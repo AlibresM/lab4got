5a775855ac7036f64ad6944e7d36e5e3316a9017
Ali Malih <43046684+AlibresM@users.noreply.github.com>
2018-12-12 20:44:15 +0300

    Add files via upload

---
+++ WpfApplication4/WpfApplication4/MainWindow.xaml.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Win32;
+using System.IO;
+
+namespace WpfApplication4
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public string FileName = "";
+
+        int snum = 0;
+        int wnum = 0;
+        public void Save()
+        {
+            if(FileName == "")
+            {
+                var dialog = new SaveFileDialog();
+                dialog.Filter = "Текстовые файлы | *.txt";
+                var result = dialog.ShowDialog();
+                if (result == true)
+                {
+                    File.WriteAllText(dialog.FileName, textBox.Text);
+                    FileName = dialog.FileName;
+                }
+            }
+            else
+            {
+                try
+                {
+                    File.WriteAllText(FileName, textBox.Text);
+                }
+                catch (Exception e)
+                {
+                    MessageBox.Show(e.Message);
+                }               
+            }
+            
+        }
+        public void SaveAs()
+        {           
+            var dialog = new SaveFileDialog();            
+            dialog.Filter = "Текстовые файлы | *.txt";           
+            var result = dialog.ShowDialog();
+            if (result == true)
+            {                
+                File.WriteAllText(dialog.FileName, textBox.Text);
+                FileName = dialog.FileName;
+            }            
+        }
+
+        public void Open()
+        {
+            var dialog = new OpenFileDialog();
+            dialog.Filter = "Текстовые файлы | *.txt";
+            var result = dialog.ShowDialog();
+            if (result == true)
+            {
+                textBox.Text = File.ReadAllText(dialog.FileName);
+                FileName = dialog.FileName;
+            }
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            Open();
+            textBox.UndoLimit = 0;
+            textBox.UndoLimit = 100;
+        }
+
+        private void MenuItem_Click_5(object sender, RoutedEventArgs e)
+        {
+            Save();
+        }
+
+        private void MenuItem_Click_1(object sender, RoutedEventArgs e)
+        {
+            SaveAs();
+        }
+
+        private void MenuItem_Click_2(object sender, RoutedEventArgs e)
+        {
+            textBox.Undo();
+        }
+
+        private void MenuItem_Click_3(object sender, RoutedEventArgs e)
+        {
+            textBox.Redo();            
+        }
+
+        private void MenuItem_Click_4(object sender, RoutedEventArgs e)
+        {       
+            var about = new Window1();           
+            about.Show();
+        }
+       
+
+        private void MenuItem_Click_6(object sender, RoutedEventArgs e)
+        {
+            var newdialog = new NewDialog();
+            newdialog.ShowDialog();
+
+            switch (newdialog.res)
+            {
+                case 0: textBox.Clear(); FileName = ""; break;
+                case 1: Save(); textBox.Clear(); FileName = ""; break;
+                case 2: break;
+                default: break;
+            }
+            textBox.UndoLimit = 0;
+            textBox.UndoLimit = 100;
+        }
+
+       
+
+        private void textBox_TextChanged_1(object sender, TextChangedEventArgs e)
+        {
+            snum = 0;
+            wnum = 0;
+            char[] textarray = textBox.Text.ToCharArray();
+
+            for (int i = 0; i < textarray.Length; i++)
+            {
+                if (textarray[i] == ' ') wnum++;
+                wordnum.Text = (wnum + 1).ToString();
+                snum = textarray.Length;
+                symbolnum.Text = snum.ToString();
+            }
+        }
+
+        private void MenuItem_Click_7(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
---
+++ WpfApplication4/WpfApplication4/NewDialog.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApplication4
+{
+    /// <summary>
+    /// Interaction logic for NewDialog.xaml
+    /// </summary>
+    public partial class NewDialog : Window
+    {
+        public NewDialog()
+        {
+            InitializeComponent();
+        }
+
+        public int res = 1;
+        private void buttonc_Click(object sender, RoutedEventArgs e)
+        {
+            res = 2;
+            Close();
+        }
+
+        private void buttony_Click(object sender, RoutedEventArgs e)
+        {
+            res = 1;
+            Close();
+        }
+
+        private void buttonn_Click(object sender, RoutedEventArgs e)
+        {
+            res = 0;
+            Close();
+        }
+    }
+}
